6aae33334fe8c2f2a1cad8d484226c55bae3e89a
wangweijun123 <262448350@qq.com>
2021-05-18 19:43:08 +0800

    Create RequestParam.java

---
+++ entry/src/main/java/com/huawei/health/bloodsugar/RequestParam.java
@@ -0,0 +1,24 @@
+package com.huawei.health.bloodsugar;
+
+public class RequestParam {
+    private int firstNum;
+    private int secondNum;
+
+    public int getFirstNum() {
+        return firstNum;
+    }
+
+    public void setFirstNum(int firstNum) {
+        this.firstNum = firstNum;
+    }
+
+    public int getSecondNum() {
+        return secondNum;
+    }
+
+    public void setSecondNum(int secondNum) {
+        this.secondNum = secondNum;
+    }
+
+
+}
