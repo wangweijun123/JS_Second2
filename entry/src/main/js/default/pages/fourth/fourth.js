a09952fecb07ff0210dafc10a1f44008efd9f782
wangweijun123 <262448350@qq.com>
2021-05-20 19:30:28 +0800

    Create fourth.js

---
+++ entry/src/main/js/default/pages/fourth/fourth.js
@@ -0,0 +1,34 @@
+// xxx.js
+export default {
+    data: {
+        // 父控件的状态变化会传给子控件，但是子控件不能作用父控件
+        isFull: false,
+        text: [{myIndex:0,content:'123'}, {myIndex:1,content:'456'}, {myIndex:2,content:'789'}],
+        itemStyle:['color-red', 'color-blue', 'color-ff']
+    },
+    onInit(){
+
+    },
+    textClicked (params) {
+        console.info("通知到了父hml -> " +params);
+//        JSON.parse()
+        console.info("通知到了父hml -> params.detail.age = " +params.detail.age);
+        console.info("通知到了父hml -> " +params.type);
+    },
+    xxxxx() {
+        console.info("通知到了父 xxxx");
+    },
+    syncChildState(params) {
+        console.info("通知到了父hml -> params.detail.showObj = " +params.detail.showObj);
+        this.isFull = params.detail.showObj;
+    },
+    changeValue() {
+        console.info("parent Clicked -->改变前 "+this.isFull);
+
+        this.isFull = !this.isFull;
+        console.info("parent 改变了..." + this.isFull);
+    },
+    spansInText() {
+
+    }
+}
