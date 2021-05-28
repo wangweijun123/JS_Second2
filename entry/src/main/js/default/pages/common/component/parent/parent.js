c8284d1282498f72e61773a234ad18bbb5f7e800
wangweijun123 <262448350@qq.com>
2021-05-28 17:52:09 +0800

    Update parent.js

--- entry/src/main/js/default/pages/common/component/parent/parent.js
+++ entry/src/main/js/default/pages/common/component/parent/parent.js
@@ -7,8 +7,25 @@ export default {
     parentClicked () {
         this.showObj = !this.showObj;
         console.info('parent component get parent text');
-        console.info(`${this.$parent().text}`);
+
+        let that = this;
+        let parent = this.$parent();
+        console.info("my print " + parent.text);
+        parent.methodInRootJS();// 其实调用了root.js中的函数methodInRootJS
+
+        console.info(`${this.$parent().text}`);// 取父控件js的data中数据,todo 调用函数
+
+        console.info("parent component get child data");
+        console.info(`${this.$child('selfDefineChild').extraData}`);
+
+        let selfDefineChild = this.$child('selfDefineChild');
+        selfDefineChild.animation();
+
         console.info("parent component get child function");
         console.info(`${this.$child('selfDefineChild').childClicked()}`);
+
+
+
+
     },
 }
