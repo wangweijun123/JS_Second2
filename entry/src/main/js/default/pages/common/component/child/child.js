b95a8e3c91665157cc79992930200f1568066dc6
wangweijun123 <262448350@qq.com>
2021-05-28 17:51:30 +0800

    Update child.js

--- entry/src/main/js/default/pages/common/component/child/child.js
+++ entry/src/main/js/default/pages/common/component/child/child.js
@@ -3,12 +3,38 @@ export default {
     data: {
         showObj: false,
         text: 'I am child component!',
+        extraData:'extra data in child js',
+        images : [
+                    { src: '/common/frame1.jpg' },
+                    { src: '/common/frame2.jpg' }
+                ]
     },
+
     childClicked () {
         this.showObj = !this.showObj;
         console.info('child component get parent text');
-        console.info('${this.$parent().text}');
+        console.info(`${this.$parent().text}`);
+
         console.info('child component get root text');
-        console.info('${this.$root().text}');
+        console.info(`${this.$root().text}`);
     },
+    animation() {
+        console.info('child component animation');
+        const animator = this.$element('animator');
+        /**
+           * Obtains the playback state. Available values are as follows:
+           * Playing
+           * Paused
+           * Stopped
+           */
+        let state = animator.getState();
+        console.info('child component state:' +state);
+        if (state === 'paused') {
+            animator.resume();
+        } else if (state === 'stopped') {
+            animator.start();
+        } else {
+            animator.pause();
+        }
+    }
 }
