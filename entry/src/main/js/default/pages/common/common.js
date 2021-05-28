5ad04749bb355bad16e0dbb2019de5bbe26f6e3a
wangweijun123 <262448350@qq.com>
2021-05-28 17:50:06 +0800

    Update common.js

--- entry/src/main/js/default/pages/common/common.js
+++ entry/src/main/js/default/pages/common/common.js
@@ -1,5 +1,10 @@
 
 const MESSAGE_CODE_START_ABILITY = 9999;
+const ABILITY_TYPE_EXTERNAL = 0;
+const ABILITY_TYPE_INTERNAL = 1;
+const BUNDLE_NAME = "com.huawei.hiaceservice";
+const ABILITY_NAME = "com.huawei.hiaceservice.CalcInternalAbility";
+
 export default {
 
     openBluetoothAdapter() {
@@ -58,5 +63,26 @@ export default {
         console.info('Start Ability result is:' + resultStr);
         let resultObj = JSON.parse(resultStr);
         console.info('Start Ability result is:' + JSON.stringify(resultObj));
-    }
+    },
+    callRemoteService: async function(callback) {
+        var actionData = {};
+        // 直接往对象里添加属性，不用声明
+        actionData.firstNum = 1024;
+        actionData.secondNum = 2048;
+        console.info("js MainAbility plus");
+        var action = {}; // 声明一个对象哦
+        // 这里必须与java服务端一致
+        action.bundleName = BUNDLE_NAME;
+        action.abilityName = ABILITY_NAME;
+        action.messageCode = 100000;
+        action.data = actionData;
+        action.abilityType = ABILITY_TYPE_INTERNAL; // 进程内的服务
+        action.syncOption = 1;
+        console.info("js MainAbility callAbility ");
+        // async 与 await 配套使用的哦
+        console.info("js MainAbility客户端发送的数据 :" + action);
+        var result = await FeatureAbility.callAbility(action);
+        console.info("js MainAbility result :" + result);
+        callback(result);
+    },
 }
