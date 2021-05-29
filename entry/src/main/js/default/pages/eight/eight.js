eb95f6a9277a3c4e1f2c3e8292fc2d790ae87502
Your Name <you@example.com>
2021-05-29 11:32:27 +0800

    update_eight

--- entry/src/main/js/default/pages/eight/eight.js
+++ entry/src/main/js/default/pages/eight/eight.js
@@ -1,11 +1,46 @@
 export default {
     data: {
         text: 'I am root!',
-        persons:[
+        persons: []
+    },
+    onInit() {
+        setTimeout(()=>{
+            this.persons = [
                 {id:1, gender:'man', age:47, name:'s1'},
                 {id:2, gender:'woman', age:25, name:'s2'},
                 {id:3, gender:'man', age:14, name:'s3'},
-        ]
+                {id:3, gender:'man', age:14, name:'s3'},
+                {id:3, gender:'man', age:14, name:'s3'},
+                {id:3, gender:'man', age:14, name:'s3'},
+                {id:3, gender:'man', age:14, name:'s3'},
+                {id:3, gender:'man', age:14, name:'s3'},
+                {id:3, gender:'man', age:14, name:'s3'},
+                {id:3, gender:'man', age:14, name:'s3'},
+                {id:3, gender:'man', age:14, name:'s3'},
+                {id:3, gender:'man', age:14, name:'s3'},
+                {id:3, gender:'man', age:14, name:'s3'},
+                {id:3, gender:'man', age:14, name:'s3'},
+                {id:3, gender:'man', age:14, name:'s3'},
+                {id:3, gender:'man', age:14, name:'s3'},
+                {id:3, gender:'man', age:14, name:'s3'},
+                {id:3, gender:'man', age:14, name:'s3'},
+                {id:3, gender:'man', age:14, name:'s3'},
+                {id:3, gender:'man', age:14, name:'s3'},
+                {id:3, gender:'man', age:14, name:'s3'},
+                {id:3, gender:'man', age:14, name:'s3'},
+                {id:3, gender:'man', age:14, name:'s3'},
+                {id:4, gender:'man', age:14, name:'s3'},
+                {id:4, gender:'man', age:14, name:'s3'},
+                {id:4, gender:'man', age:14, name:'s3'},
+                {id:4, gender:'man', age:14, name:'s3'},
+                {id:4, gender:'man', age:14, name:'s3'},
+                {id:4, gender:'man', age:14, name:'s3'},
+                {id:4, gender:'man', age:14, name:'s3'},
+                {id:4, gender:'man', age:14, name:'s3'},
+                {id:4, gender:'man', age:14, name:'s3'},
+                {id:4, gender:'man', age:14, name:'s3'},
+        ];
+        }, 2000);
     },
     onItemClick(person) {
         console.info("onItemClick "+JSON.stringify(person));
